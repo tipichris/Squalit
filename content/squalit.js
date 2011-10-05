6fd5e3f9a24e4a848aabbb3a594ba3a69efa2f12
Chris Hastie <chris@oak-wood.co.uk>
2011-10-05 21:36:02 +0100

    Preferences observer etc

--- content/squalit.js
+++ content/squalit.js
@@ -5,7 +5,7 @@
   * 1.1 (the "License"); you may not use this file except in compliance with
   * the License. You may obtain a copy of the License at
   * http://www.mozilla.org/MPL/
-  * 
+  *
   * Software distributed under the License is distributed on an "AS IS" basis,
   * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
   * for the specific language governing rights and limitations under the
@@ -32,30 +32,35 @@
   * and other provisions required by the GPL or the LGPL. If you do not delete
   * the provisions above, a recipient may use your version of this file under
   * the terms of any one of the MPL, the GPL or the LGPL.
-  * 
-  * ***** END LICENSE BLOCK ***** 
+  *
+  * ***** END LICENSE BLOCK *****
   */
 
 var squalit = {
+  isMain: false,
+
   onLoad: function() {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("squalit-strings");
     this.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.squalit.");
+    this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+    this.prefs.addObserver("", this, false);
     this.console = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
     squalit.logger(5, "Initialising");
-    
+
     this.abManager = Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
     this.promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
 
     try {
       this.aburi = this.prefs.getCharPref("addressbook");
-      this.homesuffix = this.prefs.getCharPref("homesuffix");
-      this.worksuffix = this.prefs.getCharPref("worksuffix");
-      this.cellsuffix = this.prefs.getCharPref("cellsuffix");
+      this.homesuffix = this.prefs.getComplexValue("homesuffix", Components.interfaces.nsIPrefLocalizedString).data;
+      this.worksuffix = this.prefs.getComplexValue("worksuffix", Components.interfaces.nsIPrefLocalizedString).data;
+      this.cellsuffix = this.prefs.getComplexValue("cellsuffix", Components.interfaces.nsIPrefLocalizedString).data;
       this.digits = this.prefs.getIntPref("digits");
       this.refreshint = this.prefs.getIntPref("refreshint");
+      this.dbFile = this.prefs.getComplexValue("dbfile", Components.interfaces.nsIRelativeFilePref).file;
     }
     catch(err) {
       this.logger(1, "Error retrieving preferences: " + err.message);
@@ -67,12 +72,55 @@ var squalit = {
     // document.getElementById("abResultsTree").addEventListener("select", this.onSelectNewRow, true);
 
   },
-  
+
   onLoadMain: function() {
     this.onLoad();
+    this.isMain = true;
     if (this.refreshint) {
       this.intervalID = window.setInterval(this.autoExport.bind(this), this.refreshint * 1000 * 60);
     }
+  },
+
+  observe: function(subject, topic, data) {
+      if (topic != "nsPref:changed") {
+        return;
+      }
+
+      switch(data) {
+        case "addressbook":
+          this.aburi = this.prefs.getCharPref(data);
+          this.refreshInformation();
+          break;
+
+        case "homesuffix":
+          this.homesuffix = this.prefs.getComplexValue("homesuffix", Components.interfaces.nsIPrefLocalizedString).data;
+          break;
+
+        case "cellsuffix":
+          this.cellsuffix = this.prefs.getComplexValue("cellsuffix", Components.interfaces.nsIPrefLocalizedString).data;
+          break;
+
+        case "worksuffix":
+          this.worksuffix = this.prefs.getComplexValue("worksuffix", Components.interfaces.nsIPrefLocalizedString).data;
+          break;
+
+        case "digits":
+          this.digits = this.prefs.getIntPref("digits");
+          break;
+
+        case "refreshint":
+          this.refreshint = this.prefs.getIntPref("refreshint");
+          if (this.intervalID) window.clearInterval(this.intervalID);
+          if (this.refreshint && this.isMain) {
+            this.intervalID = window.setInterval(this.autoExport.bind(this), this.refreshint * 1000 * 60);
+          }
+          break;
+
+        case "dbfile":
+          this.dbFile = this.prefs.getComplexValue("dbfile", Components.interfaces.nsIRelativeFilePref).file;
+          break;
+
+      }
   },
 
   dbConnection: null,
@@ -100,11 +148,11 @@ var squalit = {
 
   export: function () {
     this.dbInit();
-    var sAddressBook = this.abManager.getDirectory(this.aburi); 
+    var sAddressBook = this.abManager.getDirectory(this.aburi);
     this._exportBook(sAddressBook);
     this.dbConnection.asyncClose();
   },
-  
+
   autoExport: function() {
     squalit.logger(3, "Running auto export");
     this.export();
@@ -113,17 +161,17 @@ var squalit = {
   exportSelectedCards: function () {
     var cards = GetSelectedAbCards();
     if (cards != null) {
-	  this.dbInit();
-	  for (x in cards) {
-		this._exportCard(cards[x]);
-	  }
-	  this.dbConnection.asyncClose();
+      this.dbInit();
+      for (x in cards) {
+        this._exportCard(cards[x]);
+      }
+      this.dbConnection.asyncClose();
     }
   },
 
   exportSelectedDirectory: function() {
     var sABuri = GetSelectedDirectory();
-    var addressBook = this.abManager.getDirectory(sABuri); 
+    var addressBook = this.abManager.getDirectory(sABuri);
     if (addressBook != null) {
       squalit.logger(3, "Exporting from " + addressBook.dirName);
       this.dbInit();
@@ -133,19 +181,19 @@ var squalit = {
   },
 
   _exportCard: function (aCard) {
-	var num;
-	var numtypes = {"HomePhone":this.homesuffix, "WorkPhone":this.worksuffix, "CellularNumber":this.cellsuffix};
-	dName = aCard.getProperty("DisplayName", "Unknown");
-	for (ntype in numtypes) {
-	  num = aCard.getProperty(ntype, "");
-	  if (num != null && num.length >0 ) {
-		num = this.sanitizenumber(num);
-		if (num.length >0) {
-		  squalit.logger(5, dName + " " + numtypes[ntype] + ": " + num);
-		  this.dbUpdate(num, dName + " " + numtypes[ntype]);
-		}
-	  }
-	}
+    var num;
+    var numtypes = {"HomePhone":this.homesuffix, "WorkPhone":this.worksuffix, "CellularNumber":this.cellsuffix};
+    dName = aCard.getProperty("DisplayName", "Unknown");
+    for (ntype in numtypes) {
+      num = aCard.getProperty(ntype, "");
+      if (num != null && num.length >0 ) {
+        num = this.sanitizenumber(num);
+        if (num.length >0) {
+          squalit.logger(5, dName + " " + numtypes[ntype] + ": " + num);
+          this.dbUpdate(num, dName + " " + numtypes[ntype]);
+        }
+      }
+    }
   },
 
   _exportBook: function(aAddressBook) {
@@ -163,9 +211,9 @@ var squalit = {
   listbooks: function () {
     var allAddressBooks = this.abManager.directories;
 
-    while (allAddressBooks.hasMoreElements()) {  
-      var addressBook = allAddressBooks.getNext()  
-                                    .QueryInterface(Components.interfaces.nsIAbDirectory);  
+    while (allAddressBooks.hasMoreElements()) {
+      var addressBook = allAddressBooks.getNext()
+                                    .QueryInterface(Components.interfaces.nsIAbDirectory);
       if (addressBook instanceof Components.interfaces.nsIAbDirectory) {
         squalit.logger(5, "Directory Name:" + addressBook.dirName);
         squalit.logger(5, "Directory URI:" + addressBook.URI);
@@ -173,23 +221,18 @@ var squalit = {
     }
   },
 
-  dbInit: function() {  
-    var dirService = Components.classes["@mozilla.org/file/directory_service;1"].
-      getService(Components.interfaces.nsIProperties);
-
-    var dbFile = dirService.get("Home", Components.interfaces.nsIFile);
-    dbFile.append(".pyCalledMe");
-    dbFile.append("squalit2.sqlite");
+  dbInit: function() {
+    squalit.logger(3, "Exporting to " +  this.dbFile.path);
 
     var dbService = Components.classes["@mozilla.org/storage/service;1"].
       getService(Components.interfaces.mozIStorageService);
 
     var dbConnection;
 
-    if (!dbFile.exists())
-      dbConnection = this._dbCreate(dbService, dbFile);
+    if (!this.dbFile.exists())
+      dbConnection = this._dbCreate(dbService, this.dbFile);
     else {
-      dbConnection = dbService.openDatabase(dbFile);
+      dbConnection = dbService.openDatabase(this.dbFile);
     }
     this.dbConnection = dbConnection;
   },
--- defaults/preferences/squalit.js
+++ defaults/preferences/squalit.js
@@ -1,9 +1,9 @@
-pref( "extensions.squalit.dbfile", "" );
+pref( "extensions.squalit.dbfile", "[Home].callpoppy/numbers2.db" );
 pref( "extensions.squalit.addressbook", "moz-abmdbdirectory://abook.mab");
 pref( "extensions.squalit.loglevel", 5 );
-pref( "extensions.squalit.worksuffix", "(w)" );
-pref( "extensions.squalit.homesuffix", "(h)" );
-pref( "extensions.squalit.cellsuffix", "(m)" );
+pref( "extensions.squalit.worksuffix", "chrome://squalit/locale/squalit.properties" );
+pref( "extensions.squalit.homesuffix", "chrome://squalit/locale/squalit.properties" );
+pref( "extensions.squalit.cellsuffix", "chrome://squalit/locale/squalit.properties" );
 pref( "extensions.squalit.digits", 10 );
 pref( "extensions.squalit.refreshint", 2 );
 
